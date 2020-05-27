a38879302620d298ab57d7851eb0a20301953c94
pranavv481 <pranavv481@gmail.com>
2020-05-27 18:48:50 +0530

    useRef setInterval

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import ClassTimer from './components/ClassTimer';
+import HookTimer from './components/HookTimer';
+
+
+function App() {
+  return (
+
+    <div className="App">
+      <ClassTimer />
+      <HookTimer />
+    </div >
+
+  );
+}
+
+export default App;
